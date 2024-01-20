cfddcc756db1eceb8002362c3cca8b7c50ecb67c
Dualok <djfonsecamo@gmail.com>
2024-01-19 20:05:27 -0600

    Avance 3 interrupciones externas

--- laboratorios/lab2-ATtiny_Timers/pruebas/Interrupciones/interrupcion.c
+++ laboratorios/lab2-ATtiny_Timers/pruebas/Interrupciones/interrupcion.c
@@ -4,59 +4,57 @@
 #include <stdio.h>
 // Macros
 
-volatile int counter;
+
+volatile int mode;
+volatile int play;
+// Interrupt service routine for load_size_mode choice
+ISR(INT0_vect)        // Interrupt service routine 
+{
+  PORTB ^= (1<<PB3);    // Toggling the PB5 pin
+  mode++;
+}
+
+// Interrupt service routine for play/pause button
 ISR(INT1_vect)        // Interrupt service routine 
 {
-  PORTB ^= (1<<PB5);    // Toggling the PB5 pin
-  counter++;
-  _delay_ms(200);
+  PORTB ^= (1<<PB4);    // Toggling the PB5 pin
+  play++;
 }
 
-ISR(PCINT1_vect)        // Interrupt service routine 
-{
-  PORTB ^= (1<<PB5);    // Toggling the PB5 pin
-  counter++;
-  _delay_ms(200);
-}
 
-void initExternalInterrupt(){
+void initExternalInterrupts(){
 
-	//------------------ Enable external interrupt at pin B1 ------------------
+	//------------------ Enable external interrupt INT1 at pin D3 ------------------
   GIMSK |= (1<<INT1);	// enabling the INT0 (external interrupt)  
-  PCMSK |= (1 << PCINT1); // configure interrupt at PB1
-  MCUCR |= (1<<ISC11); // Configuring interrupt triggered by falling edge at PB1 
-	sei();//enabling global interrupt
-}
-
-void pinChangeInterrupt()
-{
-	sei();
-  GIMSK|= (1<<PCIE0);
-  PCMSK|=(1<<PCINT1);
+  MCUCR |= (1<<ISC11); // Configuring interrupt triggered by falling edge at PB1
   
+  //------------------ Enable external interrupt INT0 at pin D4 ------------------
+  GIMSK |= (1<<INT0);	// enabling the INT0 (external interrupt)  
+  MCUCR |= (1<<ISC01); // Configuring interrupt triggered by falling edge at PB0 
+	sei();//enabling global interrupt
 }
 
 void setup()
 {
-	// set PB6 and PB5 as output(LED)
-  DDRB |= (1<<PB6)|(1<<PB5);
-  // set PB1 as an input
-  DDRB &= ~ (1 << DDB1);
-  //initExternalInterrupt();
-  pinChangeInterrupt();
-  
+	// set PB6, PB5, PB4 and PB3 as output(LED)
+  DDRB |= (1<<PB6)|(1<<PB5)|(1<<PB4)|(1<<PB3);
+  initExternalInterrupts();
 }
 
 
 
 int main()
 {
-	counter = 0;
+	mode = 0;
   setup();
   while(1)
   {
-  	if(counter > 0){
-  		PORTB = (1<<PB6);
+  	if(mode > 3){
+  		PORTB |= (1<<PB6);
+  		_delay_ms(1000);
+  	}
+  	if(play > 3){
+  		PORTB |= (1<<PB5);
   		_delay_ms(1000);
   	}
   }
