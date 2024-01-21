07b5ff8448dc5a81fa312ee47317a7331dc24a9d
Dualok <djfonsecamo@gmail.com>
2024-01-20 18:49:33 -0600

    Integracion temporizador, interrupciones y nivel de carga

---
+++ laboratorios/lab2-ATtiny_Timers/codigo y simulacion/display7seg.c
@@ -0,0 +1,71 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+// Macros
+#define BCD3 PD0
+#define BCD2 PD1
+#define BCD1 PA1
+#define BCD0 PA0
+#define MULTIPLEXER PB7
+#define APAGADOR B5
+#define F_60HZtoDelay 133
+#define COMPARE_TIME 195
+#define SECOND_DELAY 8000
+
+/// *********************** BCD map function ******************************
+void map_bcd(int num){
+	// Clean the BCD bits
+	PORTD &= ~((1 << BCD3) | (1 << BCD2));
+	PORTA &= ~((1 << BCD1) | (1 << BCD0));
+	switch (num) {
+		case 0:
+			break;
+		case 1:
+			PORTA |= (1 << BCD0);
+		  break;
+    case 2:
+		  PORTA |= (1 << BCD1);
+		  break;
+		case 3:
+			PORTA |= ((1 << BCD1) | (1 << BCD0));
+			break;
+		case 4:
+			PORTD |= (1 << BCD2);
+			break;
+		case 5:
+			PORTD |= (1 << BCD2);
+			PORTA |= (1 << BCD0);
+		  break;
+    case 6:
+		  PORTD |= ((1 << BCD2));
+		  PORTA |= (1 << BCD1);
+		  break;
+		case 7:
+			PORTD |= (1 << BCD2);
+			PORTA |= ((1 << BCD1) | (1 << BCD0));
+			break;
+		case 8:
+			PORTD |= (1 << BCD3);
+			break;
+		case 9:
+			PORTD |= (1 << BCD3);
+			PORTA |= (1 << BCD0);
+			break;
+		default:
+			PORTD |= (1 << BCD3) | (1 << BCD2);
+      break;
+    }
+}
+
+
+/// *********************** multiplexing function ******************************
+void multiplexar(int decenas, int unidades){
+	PORTB ^= (1 << MULTIPLEXER);//toggle the bit
+	map_bcd(decenas);
+	_delay_ms(F_60HZtoDelay);
+	PORTB ^= (1 << MULTIPLEXER); //toggle the bit
+	map_bcd(unidades);
+	_delay_ms(F_60HZtoDelay); 
+}
--- laboratorios/lab2-ATtiny_Timers/codigo y simulacion/wash_machine.c
+++ laboratorios/lab2-ATtiny_Timers/codigo y simulacion/wash_machine.c
@@ -3,31 +3,33 @@
 #include <util/delay.h>
 #include <stdio.h>
 
-// Macros
-#define BCD3 PD0
-#define BCD2 PD1
-#define BCD1 PD2
-#define BCD0 PD3
-#define MULTIPLEXER PB7
-#define APAGADOR B5
-#define F_60HZtoDelay 133
+// ***************** local headers ***********************
+#include "display7seg.h"
+
+// ***************** Macros ******************************
 #define COMPARE_TIME 195
-//#define F_CPU 1600000
-#define SECOND_DELAY 8000
+#define LOAD_SIZE_BIT1 PB3
+#define LOAD_SIZE_BIT2 PB4
+#define SWITCH_7SEG PB6
 
 // ************************ Global Variables ****************************
+volatile int mode;
+volatile int play;
 volatile int ticks_per_second;
 volatile int time_left;
-/// *********************** Function prototypes *************************
+volatile unsigned char pause_flag;
+
+/// *********************** Function Prototypes *************************
 void setup(void);
-void map_bcd(int);
-void multiplexar(int , int );
+void initTimerInterrupt(void);
+void initExternalInterrupts(void);
+void loadSize(int);
+void pauseHandler(void);
 
-
-/// ********************** Interruption *********************************
+/// ********************** Timer 40hz interrupt *************************
 ISR(TIMER0_COMPA_vect){
 	if(ticks_per_second == 0){
-		PORTB ^= (1<<PORTB6);
+		PORTB ^= (1<<PORTB5);
 		ticks_per_second = 40;
 		time_left--;
 	}
@@ -36,6 +38,25 @@ ISR(TIMER0_COMPA_vect){
 	}
 
 }
+
+// Interrupt service routine for load_size_mode choice
+ISR(INT0_vect)        // Interrupt service routine 
+{
+  pause_flag++;
+  if(pause_flag > 1){
+  	pause_flag = 0;
+  }
+}
+
+// Interrupt service routine for play/pause button
+ISR(INT1_vect)        // Interrupt service routine 
+{
+  mode++; // increase the mode
+  if(mode > 3){
+  	mode = 0;
+  }
+}
+
 /// *********************** main function ******************************
 int main(void)
 {
@@ -43,25 +64,33 @@ int main(void)
 	unsigned char units;
 	ticks_per_second = 40;
 	time_left = 30;
+	pause_flag = 1;
 	setup();
 	
   while (1) {
-  	tens = time_left/10;
-  	units = time_left%10;
-    multiplexar(tens,units);
-    
+		pauseHandler();
+		tens = time_left/10;
+		units = time_left%10;
+		multiplexar(tens,units);
   }
 }
 
-
 /// *********************** Setup function ******************************
 void setup(){
-	//---- D3, D2, D1, D0 as outputs ---------
-	DDRD = (1 << DDD3) | (1 << DDD2) | (1 << DDD1) | (1 << DDD0) | (1 << DDD3);
+	//---- A1, A0, D1, D0 as outputs ---------
+	DDRD = (1 << DDD1) | (1 << DDD0);
+	DDRA = (1 << DDA1) | (1 << DDA0);
 	
-	//---- B7, B6 s outputs ------------------
-	DDRD = (1 << DDB7) | (1 << DDB6) | (1 << DDB5);
+	//---- B7, B6, B5, B4 and B3 as outputs ------------------
+	DDRB = (1 << DDB7) | (1 << DDB6) | (1 << DDB5) | (1 << DDB4) | (1 << DDB3);
 	
+	// ----------------- Enable interrupts ----------------------
+	initTimerInterrupt();
+	initExternalInterrupts();
+}
+
+/// ************************ Timer interrupt ****************************
+void initTimerInterrupt(){
 	// -------- Timer0 setup -----------------
 	TCCR0A = (1 << WGM01); // CTC mode - Clear timer on compare
 	OCR0A = COMPARE_TIME; // Generate 40Hz interruption
@@ -70,54 +99,52 @@ void setup(){
 	sei();
 }
 
+/// *********************** External interrupts *************************
+void initExternalInterrupts(){
+	//------------------ Enable external interrupt INT1 at pin D3 ------------------
+  GIMSK |= (1<<INT1);	// enabling the INT0 (external interrupt)  
+  MCUCR |= (1<<ISC11); // Configuring interrupt triggered by falling edge at PB1
+  
+  //------------------ Enable external interrupt INT0 at pin D4 ------------------
+  GIMSK |= (1<<INT0);	// enabling the INT0 (external interrupt)  
+  MCUCR |= (1<<ISC01); // Configuring interrupt triggered by falling edge at PB0 
+	sei();//enabling global interrupt
+}
 
-/// *********************** BCD map function ******************************
-void map_bcd(int num){
-	// Clean the BCD bits
-	PORTD &= ~((1 << BCD3) | (1 << BCD2) | (1 << BCD1) | (1 << BCD0));
-	switch (num) {
-		case 0:
-			break;
+/// ********************* Size load options *****************************
+void loadSize(int nivel){
+	// clean bits
+	PORTB &= ~((1 << LOAD_SIZE_BIT1) | (1 << LOAD_SIZE_BIT2));
+	switch(nivel){
+		// low size load
 		case 1:
-			PORTD |= (1 << BCD0);
-		  break;
-    case 2:
-		  PORTD |= (1 << BCD1);
-		  break;
+			PORTB |= (1 << LOAD_SIZE_BIT1);
+			break;
+		// medium size load
+		case 2:
+			PORTB |= (1 << LOAD_SIZE_BIT2);
+			break;
+		// high size load
 		case 3:
-			PORTD |= ((1 << BCD1) | (1 << BCD0));
-			break;
-		case 4:
-			PORTD |= (1 << BCD2);
-			break;
-		case 5:
-			PORTD |= ((1 << BCD2) | (1 << BCD0));
-		  break;
-    case 6:
-		  PORTD |= ((1 << BCD2) | (1 << BCD1));
-		  break;
-		case 7:
-			PORTD |= ((1 << BCD2) | (1 << BCD1) | (1 << BCD0));
-			break;
-		case 8:
-			PORTD |= (1 << BCD3);
-			break;
-		case 9:
-			PORTD |= ((1 << BCD3) | (1 << BCD0));
+			PORTB |= (1 << LOAD_SIZE_BIT1) | (1 << LOAD_SIZE_BIT2);
 			break;
 		default:
-			// Hacer algo por default
-      break;
-    }
+			break;
+	}
+}
+
+void pauseHandler(){
+	if(pause_flag == 1){
+		//pause, disable compare match A
+		TIMSK &= ~(1 << OCIE0A);
+		// load size selection only available on pause
+		loadSize(mode);
+	}
+	//play, enable compare match A
+	else if(pause_flag == 0) {
+		TIMSK = (1 << OCIE0A);
+		PORTB |= (1<<SWITCH_7SEG); //turn on the 7 segments
+	}
 }
 
 
-/// *********************** multiplexing function ******************************
-void multiplexar(int decenas, int unidades){
-	PORTB ^= (1 << MULTIPLEXER);//toggle the bit
-	map_bcd(decenas);
-	_delay_ms(F_60HZtoDelay);
-	PORTB ^= (1 << MULTIPLEXER); //toggle the bit
-	map_bcd(unidades);
-	_delay_ms(F_60HZtoDelay); 
-}
