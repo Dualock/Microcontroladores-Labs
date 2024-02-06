90b852a653ab7eb415abd95847729bf3e679dd03
koalami <56325974+koalami@users.noreply.github.com>
2024-02-06 15:11:46 -0600

    Add files via upload

--- laboratorios/lab4-STM32F429/TEST_IOT/IoT_Data.py
+++ laboratorios/lab4-STM32F429/TEST_IOT/IoT_Data.py
@@ -13,25 +13,25 @@ def connect_True(client, userdata, flags, rc):
 # Función para manejar los mensajes recibidos
 def on_message(client, userdata, message):
     print(f"Mensaje recibido: {message.payload.decode()}")
-
-# Configuración del cliente MQTT
-broker = "iot.eie.ucr.ac.cr"
-port = 1883  
-token = "5pavshof7n9xx2kx8zee"  # Token de autenticación
-username = "Sismo_STM32"
 # Configuración del puerto serial
 serial_connection =  serial.Serial(
-    port="COM1", 
+    port="COM4", 
     baudrate=115200, 
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
     timeout=1)
+# Configuración del cliente MQTT
+broker = "iot.eie.ucr.ac.cr"
+port = 1883  
+token = "5pavshof7n9xx2kx8zee"  # Token de autenticación
+username = "Sismo_STM32"
+topic = "v1/devices/me/telemetry"
  
-
+data = dict()
 # Crear instancia del cliente MQTT
 client = mqtt.Client()
-client.username_pw_set(token)
+client.username_pw_set(device)
 
 client.on_message = on_message
 
@@ -49,26 +49,26 @@ try:
     while True:
         if serial_connection.in_waiting > 0:
             line = serial_connection.readline().decode('utf-8').strip()
-            values = line.split("\t")
+            components = line.split(',')  # Divide la línea en sus componentes individuales
 
-            # Crear un diccionario con los datos
-            data = {
-                "DIRx": float(values[0]),
-                "DIRy": float(values[1]),
-                "DIRz": float(values[2]),
-                "battery": float(values[3])
-            }
+            # Inicializa el diccionario de datos
+            if len(data_captured) >= 3:
+                data['x'] = data_captured[0]
+                data['y'] = data_captured[1]
+                data['z'] = data_captured[2]
+                 
 
-            # Publicar los datos en el topic deseado
+            data['Battery']= data_captured[3]
+            # Publica los datos en el topic deseado
+            output = json.dumps(data)
             client.publish("v1/devices/me/telemetry", json.dumps(data), qos=1)
             time.sleep(1)
 except serial.SerialException as e:
     print(f"Error en la conexión serial: {str(e)}")
 except KeyboardInterrupt:
-        # Interrupción por teclado (Ctrl + C).
+    # Interrupción por teclado (Ctrl + C).
     print("\nPrograma terminado. Datos guardados de", username)
 finally:
-# Detener el bucle de mensajes MQTT y cerrar la conexión serial
+    # Detener el bucle de mensajes MQTT y cerrar la conexión serial
     client.loop_stop()
     serial_connection.close()
-# Bucle principal
