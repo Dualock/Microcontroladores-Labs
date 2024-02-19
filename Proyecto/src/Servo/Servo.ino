1d2dca1bdeb2c1e46eaa5bb86c91d50096a5b0df
Dualok <djfonsecamo@gmail.com>
2024-02-19 07:08:31 -0600

    Desacople y maquina de estados

--- Proyecto/src/Servo/Servo.ino
+++ Proyecto/src/Servo/Servo.ino
@@ -2,25 +2,26 @@
 #include <Servo.h>
 #include "state_machine.h"
 
+// Definicion de Pines
 int led_pin = 13; // LED naranja del arduino NANO BLE33
-int led_closed = 9;
-int led_open = 8;
 int openclose_btn = 3;
-int position = 0;
 int action = 0; // 0 para cerrar, 1 para abrir
-int RED_LED_TEST = 7;
-Servo myservo;
+
+//
+States currentState = ST_IDLE;
+Events event = EV_NONE;
+
 // Prototipos de funciones
 
+// Variables globales
+static int current_pos = 180;
 void setup() {
-  myservo.attach(11);
-  pinMode(led_pin, OUTPUT);
-  pinMode(led_closed, OUTPUT);
-  pinMode(led_open, OUTPUT);
-  pinMode(RED_LED_TEST, OUTPUT);
+  void setup_fsm();
   pinMode(openclose_btn, INPUT);
   attachInterrupt(digitalPinToInterrupt(openclose_btn), openclose_state, FALLING);
+  eventHandler(currentState, event, current_pos);
 }
+// Interrupcion para generar eventos de apertura y cierre
 void openclose_state(){
   if(action == 0){
     action = 1;
@@ -29,49 +30,19 @@ void openclose_state(){
     action = 0;
     }
   }
-int open_gate(int current_position){
-  while(current_position > 0) {
-    current_position--;
-    myservo.write(current_position);
-    delay(20);                       // Se mueve 1 grado por cada 20ms
-  }
-  return current_position;
-}
 
-int close_gate(int current_position){
-  position = current_position;
-  while(current_position < 180) {
-    current_position++;
-    myservo.write(current_position);
-    delay(20);
-  }
-  return current_position;
-}
-  
+
 void loop() {
-  int current_pos = 180;
-  setup();
-  while(1){
-    if(action == 0){
-      current_pos = close_gate(current_pos);
+  if(action == 0){
+   event = EV_CLOSE;
+   current_pos = eventHandler(currentState, event, current_pos);
+  }
+  else if(action == 1){
+    event = EV_CLOSE;
+    current_pos = eventHandler(currentState, event, current_pos);
     }
-    else if(action == 1){
-      current_pos = open_gate(current_pos);
-      }
-      // Encender LED de abierto
-    if(current_pos == 0){
-      digitalWrite(led_open, HIGH);
-      digitalWrite(led_closed, LOW);
-      }
-    // Encender LED de cerrado
-    else if(current_pos == 180){
-      digitalWrite(led_closed, HIGH);
-      digitalWrite(led_open, LOW);
-      }
-      Serial.println(action);
-      blink(RED_LED_TEST);
-      delay(100);
-    }
-    
-
+  digitalWrite(led_pin, HIGH);
+  delay(500);
+  digitalWrite(led_pin, HIGH);
+  delay(500);
 }
--- Proyecto/src/Servo/state_machine.h
+++ Proyecto/src/Servo/state_machine.h
@@ -1,4 +1,28 @@
 #ifndef STATE_MACHINE_H
 #define STATE_MACHINE_H
-void blink(int RED_LED);
+
+// Definición de los estados
+enum States {
+  ST_IDLE,
+  ST_CLOSED,
+  ST_OPENNING,
+  ST_OPENED,
+  ST_CLOSING,
+  ST_STOPPED
+};
+
+enum Events {
+  EV_NONE,
+  EV_DONE,
+  EV_OPEN,
+  EV_CLOSE,
+};
+void setup_fsm();
+
+int eventHandler(States &currentState, Events &event, int current_pos);
+
+int open_gate(int current_position);
+
+int close_gate(int current_position);
+
 #endif 
